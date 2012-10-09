ec21a17e7d47e98f8fee4560fffdd825d31f194f
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2012-10-09 05:36:25 -0400

    testing almost done

--- door_wood_plain.lua
+++ door_wood_plain.lua
@@ -73,7 +73,7 @@ minetest.register_node("homedecor:door_wood_plain_bottom_left", {
 		local pos = pointed_thing.above
 		local pd = true
 		if type(IsPlayerNodeOwner)=="function" then
-			if not IsPlayerNodeOwner(pos, placer:get_player_name()) and getLastOwner(pos) ~= "< Unknown >" then
+			if not IsPlayerNodeOwner(pos, placer:get_player_name()) and HasOwner(pos) then
 				minetest.chat_send_player( placer:get_player_name(), "Sorry, "..getLastOwner(pos).." owns that spot." )
 				pd = false
 			end
@@ -175,7 +175,7 @@ minetest.register_node("homedecor:door_wood_plain_bottom_right", {
 		local pos = pointed_thing.above
 		local pd = true
 		if type(IsPlayerNodeOwner)=="function" then
-			if not IsPlayerNodeOwner(pos, placer:get_player_name()) and getLastOwner(pos) ~= "< Unknown >" then
+			if not IsPlayerNodeOwner(pos, placer:get_player_name()) and HasOwner(pos) then
 				minetest.chat_send_player( placer:get_player_name(), "Sorry, "..getLastOwner(pos).." owns that spot." )
 				pd = false
 			end
