d9f56ea229658aa0f34ec4e1fd621af1634eaa4f
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-05-29 19:13:23 -0400

    check for presence of moreblocks before overriding empty bookshelf

--- homedecor_3d_extras/init.lua
+++ homedecor_3d_extras/init.lua
@@ -56,27 +56,29 @@ minetest.override_item("default:bookshelf", {
 	}
 })
 
-minetest.override_item("moreblocks:empty_bookshelf", {
-	drawtype = "nodebox",
-	tiles = {
-		"3dbookshelf_top.png",
-		"3dbookshelf_bottom.png",
-		"3dbookshelf_sides.png",
-		"3dbookshelf_sides.png",
-		"3dbookshelf_fb_empty.png",
-		"3dbookshelf_fb_empty.png"
-	},
-	paramtype = "light",
-	paramtype2 = "facedir",
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.1875, 0.5, 0.5, 0.1875}, -- NodeBox1
-			{-0.5, -0.5, -0.5, -0.4375, 0.5, 0.5}, -- NodeBox2
-			{0.4375, -0.5, -0.5, 0.5, 0.5, 0.5}, -- NodeBox3
-			{-0.5, 0.4375, -0.5, 0.5, 0.5, 0.5}, -- NodeBox4
-			{-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5}, -- NodeBox5
-			{-0.5, -0.0625, -0.5, 0.5, 0.0625, 0.5}, -- NodeBox6
+if minetest.get_modpath("moreblocks") then
+	minetest.override_item("moreblocks:empty_bookshelf", {
+		drawtype = "nodebox",
+		tiles = {
+			"3dbookshelf_top.png",
+			"3dbookshelf_bottom.png",
+			"3dbookshelf_sides.png",
+			"3dbookshelf_sides.png",
+			"3dbookshelf_fb_empty.png",
+			"3dbookshelf_fb_empty.png"
+		},
+		paramtype = "light",
+		paramtype2 = "facedir",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				{-0.5, -0.5, -0.1875, 0.5, 0.5, 0.1875}, -- NodeBox1
+				{-0.5, -0.5, -0.5, -0.4375, 0.5, 0.5}, -- NodeBox2
+				{0.4375, -0.5, -0.5, 0.5, 0.5, 0.5}, -- NodeBox3
+				{-0.5, 0.4375, -0.5, 0.5, 0.5, 0.5}, -- NodeBox4
+				{-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5}, -- NodeBox5
+				{-0.5, -0.0625, -0.5, 0.5, 0.0625, 0.5}, -- NodeBox6
+			}
 		}
-	}
-})
+	})
+end
