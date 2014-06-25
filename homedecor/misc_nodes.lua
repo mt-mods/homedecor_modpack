d1c01d3dff660da08959b32d502fb80194afe629
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-06-25 01:27:27 -0400

    Added bathroom/kitchen floor tiles in four shades

--- homedecor/misc_nodes.lua
+++ homedecor/misc_nodes.lua
@@ -277,3 +277,64 @@ minetest.register_node('homedecor:air_conditioner', {
 	},
 	selection_box = { -0.5, -0.5, -0.5, 0.5, 0.5, 0.5 }
 })
+
+minetest.register_node("homedecor:tiles_1", {
+	description = "Bathroom/kitchen tiles (shade #1)",
+	tiles = {
+		"homedecor_tiles1.png",
+		"homedecor_tiles1.png",
+		"homedecor_tiles1.png",
+		"homedecor_tiles1.png",
+		"homedecor_tiles1.png^[transformR90",
+		"homedecor_tiles1.png^[transformR90"
+	},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("homedecor:tiles_2", {
+	description = "Bathroom/kitchen tiles (shade #2)",
+	tiles = {
+		"homedecor_tiles2.png",
+		"homedecor_tiles2.png",
+		"homedecor_tiles2.png",
+		"homedecor_tiles2.png",
+		"homedecor_tiles2.png^[transformR90",
+		"homedecor_tiles2.png^[transformR90"
+	},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("homedecor:tiles_3", {
+	description = "Bathroom/kitchen tiles (shade #3)",
+	tiles = {
+		"homedecor_tiles3.png",
+		"homedecor_tiles3.png",
+		"homedecor_tiles3.png",
+		"homedecor_tiles3.png",
+		"homedecor_tiles3.png^[transformR90",
+		"homedecor_tiles3.png^[transformR90"
+	},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("homedecor:tiles_4", {
+	description = "Bathroom/kitchen tiles (shade #4)",
+	tiles = {
+		"homedecor_tiles4.png",
+		"homedecor_tiles4.png",
+		"homedecor_tiles4.png",
+		"homedecor_tiles4.png",
+		"homedecor_tiles4.png^[transformR90",
+		"homedecor_tiles4.png^[transformR90"
+	},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults(),
+})
+
