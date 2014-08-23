5405b71a0510ca84582c65550f788409535dca6a
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-08-23 16:22:07 -0400

    fix positioning of homedecor doorbell sound

--- homedecor/misc-nodes.lua
+++ homedecor/misc-nodes.lua
@@ -1117,6 +1117,7 @@ minetest.register_node("homedecor:doorbell", {
 	},
 	on_punch = function(pos, node, puncher, pointed_thing)
 		minetest.sound_play("homedecor_doorbell", {
+			pos = pos,
 			gain = 1.0,
 			max_hear_distance = 15
 		}) 
