83557888ae40d334a815765b1344561d00b39cb0
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-08-25 18:56:48 -0400

    add brass versions of chains' items

--- chains/init.lua
+++ chains/init.lua
@@ -1,7 +1,9 @@
 print("[Chains] v1.1")
 
+-- wrought iron items
+
 minetest.register_node("chains:chain", {
-	description = "Hanging chain",
+	description = "Hanging chain (wrought iron)",
 	walkable = false,
 	climbable = true,
 	sunlight_propagates = true,
@@ -15,7 +17,7 @@ minetest.register_node("chains:chain", {
 })
 
 minetest.register_node("chains:chain_top", {
-	description = "Hanging chain (ceiling mount)",
+	description = "Hanging chain (ceiling mount, wrought iron)",
 	walkable = false,
 	climbable = true,
 	sunlight_propagates = true,
@@ -29,7 +31,7 @@ minetest.register_node("chains:chain_top", {
 })
 
 minetest.register_node("chains:chandelier", {
-	description = "Chandelier",
+	description = "Chandelier (wrought iron)",
 	paramtype = "light",
 	walkable = false,
 	light_source = LIGHT_MAX-2,
@@ -43,6 +45,54 @@ minetest.register_node("chains:chandelier", {
 	groups = {cracky=3},
 	sounds =  default.node_sound_stone_defaults(),
 })
+
+-- brass-based items
+
+minetest.register_node("chains:chain_brass", {
+	description = "Hanging chain (brass)",
+	walkable = false,
+	climbable = true,
+	sunlight_propagates = true,
+	paramtype = "light",
+	drops = "",
+	tiles = { "chains_chain_brass.png" },
+	inventory_image = "chains_chain_brass.png",
+	drawtype = "plantlike",
+	groups = {cracky=3},
+	sounds =  default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("chains:chain_top_brass", {
+	description = "Hanging chain (ceiling mount, brass)",
+	walkable = false,
+	climbable = true,
+	sunlight_propagates = true,
+	paramtype = "light",
+	drops = "",
+	tiles = { "chains_chain_top_brass.png" },
+	inventory_image = "chains_chain_top_brass_inv.png",
+	drawtype = "plantlike",
+	groups = {cracky=3},
+	sounds =  default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("chains:chandelier_brass", {
+	description = "Chandelier (brass)",
+	paramtype = "light",
+	walkable = false,
+	light_source = LIGHT_MAX-2,
+	climbable = true,
+	sunlight_propagates = true,
+	paramtype = "light",
+	drops = "",
+	tiles = { {name="chains_chandelier_brass.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=3.0}}},
+	inventory_image = "chains_chandelier_brass_inv.png",
+	drawtype = "plantlike",
+	groups = {cracky=3},
+	sounds =  default.node_sound_stone_defaults(),
+})
+
+-- crafts
 
 minetest.register_craft({
 	output = 'chains:chain 2',
@@ -69,3 +119,32 @@ minetest.register_craft({
 		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
 	}
 })
+
+-- brass versions
+
+minetest.register_craft({
+	output = 'chains:chain_brass 2',
+	recipe = {
+		{'homedecor:chainlink_brass'},
+		{'homedecor:chainlink_brass'},
+		{'homedecor:chainlink_brass'},
+	}
+})
+
+minetest.register_craft({
+	output = 'chains:chain_top_brass',
+	recipe = {
+		{'technic:brass_ingot'},
+		{'homedecor:chainlink_brass'},	
+	},
+})
+
+minetest.register_craft({
+	output = 'chains:chandelier_brass',
+	recipe = {
+		{'', 'homedecor:chainlink_brass', ''},
+		{'default:torch', 'homedecor:chainlink_brass', 'default:torch'},
+		{'technic:brass_ingot', 'technic:brass_ingot', 'technic:brass_ingot'},
+	}
+})
+
