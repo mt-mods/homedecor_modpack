63e697cbb66f1ee06512b093d341678237ea3010
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2014-09-30 13:17:43 -0400

    give coat tree an inv image

--- homedecor/misc-nodes.lua
+++ homedecor/misc-nodes.lua
@@ -976,14 +976,14 @@ minetest.register_node("homedecor:coatrack_wallmount", {
 	},
 })
 
-
 minetest.register_node("homedecor:coat_tree", {
 	tiles = { "homedecor_coatrack_wood.png" },
+	inventory_image = "homedecor_coatrack_inv.png",
 	description = "Coat tree",
 	drawtype = "nodebox",
 	paramtype = "light",
-        paramtype2 = "facedir",
-        groups = {snappy=3},
+	paramtype2 = "facedir",
+	groups = {snappy=3},
 	node_box = {
 		type = "fixed",
 		fixed = {
