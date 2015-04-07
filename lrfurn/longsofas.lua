2b5d07470a992edd21a28eca19e79caf692a4bf6
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2015-04-06 21:35:25 -0400

    use mesh nodes for lrfurn sofas and armchairs

--- lrfurn/longsofas.lua
+++ lrfurn/longsofas.lua
@@ -14,102 +14,24 @@ for i in ipairs(longsofas_list) do
 	local longsofadesc = longsofas_list[i][1]
 	local colour = longsofas_list[i][2]
 
-	minetest.register_node("lrfurn:longsofa_right_"..colour, {
+	minetest.register_node("lrfurn:longsofa_"..colour, {
 		description = longsofadesc,
-		drawtype = "nodebox",
-		tiles = {"lrfurn_sofa_right_top_"..colour..".png", "lrfurn_coffeetable_back.png",  "lrfurn_sofa_right_front_"..colour..".png",  "lrfurn_sofa_back_"..colour..".png",  "lrfurn_sofa_left_side_"..colour..".png",  "lrfurn_sofa_right_side_"..colour..".png"},
+		drawtype = "mesh",
+		mesh = "lrfurn_sofa_long.obj",
+		tiles = {
+			"lrfurn_sofa_"..colour..".png",
+			"lrfurn_sofa_bottom.png"
+		},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
 		sounds = default.node_sound_wood_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-						--legs
-						{-0.4375, -0.5, -0.4375, -0.375, -0.375, -0.375},
-						{0.375, -0.5, -0.4375, 0.4375, -0.375, -0.375},
-
-						--base/cushion
-						{-0.5, -0.375, -0.5, 0.5, 0, 0.5},
-
-						--back
-						{-0.5, 0, -0.5, -0.3125, 0.5, 0.5},
-
-						--arm
-						{-0.3125, 0, -0.5, 0.5, 0.25, -0.3125},
-					}
-		},
 		selection_box = {
 			type = "fixed",
 			fixed = {
-						{-0.5, -0.5, -0.5, 0.5, 0.5, 2.5},
-					}
+				{-0.5, -0.5, -0.5, 0.5, 0.5, 2.5},
+			}
 		},
-
-		on_construct = function(pos)
-			local node = minetest.get_node(pos)
-			local param2 = node.param2
-			node.name = "lrfurn:longsofa_middle_"..colour
-			if param2 == 0 then
-				pos.z = pos.z+1
-			elseif param2 == 1 then
-				pos.x = pos.x+1
-			elseif param2 == 2 then
-				pos.z = pos.z-1
-			elseif param2 == 3 then
-				pos.x = pos.x-1
-			end
-			if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "air" ) then
-				minetest.set_node(pos, node)
-				node.name = "lrfurn:longsofa_left_"..colour
-				if param2 == 0 then
-					pos.z = pos.z+1
-				elseif param2 == 1 then
-					pos.x = pos.x+1
-				elseif param2 == 2 then
-					pos.z = pos.z-1
-				elseif param2 == 3 then
-					pos.x = pos.x-1
-				end
-				if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "air" ) then
-					minetest.set_node(pos, node)
-				end
-			end
-		end,
-
-		on_destruct = function(pos)
-			local node = minetest.get_node(pos)
-			local param2 = node.param2
-			if param2 == 0 then
-				pos.z = pos.z+1
-			elseif param2 == 1 then
-				pos.x = pos.x+1
-			elseif param2 == 2 then
-				pos.z = pos.z-1
-			elseif param2 == 3 then
-				pos.x = pos.x-1
-			end
-			if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "lrfurn:longsofa_middle_"..colour ) then
-				if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).param2 == param2 ) then
-					minetest.remove_node(pos)
-					if param2 == 0 then
-						pos.z = pos.z+1
-					elseif param2 == 1 then
-						pos.x = pos.x+1
-					elseif param2 == 2 then
-						pos.z = pos.z-1
-					elseif param2 == 3 then
-						pos.x = pos.x-1
-					end
-					if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "lrfurn:longsofa_left_"..colour ) then
-						if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).param2 == param2 ) then
-							minetest.remove_node(pos)
-						end
-					end
-				end
-			end
-		end,
-
 		on_rightclick = function(pos, node, clicker)
 			if not clicker:is_player() then
 				return
@@ -120,68 +42,9 @@ for i in ipairs(longsofas_list) do
 		end
 	})
 
-	minetest.register_node("lrfurn:longsofa_middle_"..colour, {
-		drawtype = "nodebox",
-		tiles = {"lrfurn_longsofa_middle_top_"..colour..".png", "lrfurn_coffeetable_back.png",  "lrfurn_longsofa_middle_front_"..colour..".png",  "lrfurn_sofa_back_"..colour..".png",  "lrfurn_sofa_left_side_"..colour..".png",  "lrfurn_sofa_right_side_"..colour..".png"},
-		paramtype = "light",
-		paramtype2 = "facedir",
-		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
-		sounds = default.node_sound_wood_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-						--legs
-						{-0.4375, -0.5, -0.03125, -0.375, -0.375, 0.03125},
-						{0.375, -0.5, -0.03125, 0.4375, -0.375, 0.03125},
-
-						--base/cushion
-						{-0.5, -0.375, -0.5, 0.5, 0, 0.5},
-
-						--back
-						{-0.5, 0, -0.5, -0.3125, 0.5, 0.5},
-					}
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-						{0, 0, 0, 0, 0, 0},
-					}
-		},
-	})
-
-	minetest.register_node("lrfurn:longsofa_left_"..colour, {
-		drawtype = "nodebox",
-		tiles = {"lrfurn_sofa_left_top_"..colour..".png", "lrfurn_coffeetable_back.png",  "lrfurn_sofa_left_front_"..colour..".png",  "lrfurn_sofa_back_"..colour..".png",  "lrfurn_sofa_left_side_"..colour..".png",  "lrfurn_sofa_right_side_"..colour..".png"},
-		paramtype = "light",
-		paramtype2 = "facedir",
-		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
-		sounds = default.node_sound_wood_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-						--legs
-						{-0.4375, -0.5, 0.375, -0.375, -0.375, 0.4375},
-						{0.375, -0.5, 0.375, 0.4375, -0.375, 0.4375},
-
-						--base/cushion
-						{-0.5, -0.375, -0.5, 0.5, 0, 0.5},
-
-						--back
-						{-0.5, 0, -0.5, -0.3125, 0.5, 0.5},
-
-						--arm
-						{-0.3125, 0, 0.3125, 0.5, 0.25, 0.5},
-					}
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-						{0, 0, 0, 0, 0, 0},
-					}
-		},
-	})
-
-	minetest.register_alias("lrfurn:longsofa_"..colour, "lrfurn:longsofa_right_"..colour)
+	minetest.register_alias("lrfurn:longsofa_left_"..colour, "air")
+	minetest.register_alias("lrfurn:longsofa_middle_"..colour, "air")
+	minetest.register_alias("lrfurn:longsofa_right_"..colour, "lrfurn:longsofa_"..colour)
 
 	minetest.register_craft({
 		output = "lrfurn:longsofa_"..colour,
--- lrfurn/sofas.lua
+++ lrfurn/sofas.lua
@@ -14,74 +14,24 @@ for i in ipairs(sofas_list) do
 	local sofadesc = sofas_list[i][1]
 	local colour = sofas_list[i][2]
 
-	minetest.register_node("lrfurn:sofa_right_"..colour, {
+	minetest.register_node("lrfurn:sofa_"..colour, {
 		description = sofadesc,
-		drawtype = "nodebox",
-		tiles = {"lrfurn_sofa_right_top_"..colour..".png", "lrfurn_coffeetable_back.png",  "lrfurn_sofa_right_front_"..colour..".png",  "lrfurn_sofa_back_"..colour..".png",  "lrfurn_sofa_left_side_"..colour..".png",  "lrfurn_sofa_right_side_"..colour..".png"},
+		drawtype = "mesh",
+		mesh = "lrfurn_sofa_short.obj",
+		tiles = {
+			"lrfurn_sofa_"..colour..".png",
+			"lrfurn_sofa_bottom.png"
+		},
 		paramtype = "light",
 		paramtype2 = "facedir",
 		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
 		sounds = default.node_sound_wood_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-						--legs
-						{-0.4375, -0.5, -0.4375, -0.375, -0.375, -0.375},
-						{0.375, -0.5, -0.4375, 0.4375, -0.375, -0.375},
-
-						--base/cushion
-						{-0.5, -0.375, -0.5, 0.5, 0, 0.5},
-
-						--back
-						{-0.5, 0, -0.5, -0.3125, 0.5, 0.5},
-
-						--arm
-						{-0.3125, 0, -0.5, 0.5, 0.25, -0.3125},
-					}
-		},
 		selection_box = {
 			type = "fixed",
 			fixed = {
 						{-0.5, -0.5, -0.5, 0.5, 0.5, 1.5},
 					}
 		},
-
-		on_construct = function(pos)
-			local node = minetest.get_node(pos)
-			local param2 = node.param2
-			node.name = "lrfurn:sofa_left_"..colour
-			if param2 == 0 then
-				pos.z = pos.z+1
-			elseif param2 == 1 then
-				pos.x = pos.x+1
-			elseif param2 == 2 then
-				pos.z = pos.z-1
-			elseif param2 == 3 then
-				pos.x = pos.x-1
-			end
-			if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "air" ) then
-				minetest.set_node(pos, node)
-			end
-		end,
-
-		on_destruct = function(pos)
-			local node = minetest.get_node(pos)
-			local param2 = node.param2
-			if param2 == 0 then
-				pos.z = pos.z+1
-			elseif param2 == 1 then
-				pos.x = pos.x+1
-			elseif param2 == 2 then
-				pos.z = pos.z-1
-			elseif param2 == 3 then
-				pos.x = pos.x-1
-			end
-			if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).name == "lrfurn:sofa_left_"..colour ) then
-				if( minetest.get_node({x=pos.x, y=pos.y, z=pos.z}).param2 == param2 ) then
-					minetest.remove_node(pos)
-				end
-			end
-		end,
 
 		on_rightclick = function(pos, node, clicker)
 			if not clicker:is_player() then
@@ -93,39 +43,8 @@ for i in ipairs(sofas_list) do
 		end
 	})
 
-	minetest.register_node("lrfurn:sofa_left_"..colour, {
-		drawtype = "nodebox",
-		tiles = {"lrfurn_sofa_left_top_"..colour..".png", "lrfurn_coffeetable_back.png",  "lrfurn_sofa_left_front_"..colour..".png",  "lrfurn_sofa_back_"..colour..".png",  "lrfurn_sofa_left_side_"..colour..".png",  "lrfurn_sofa_right_side_"..colour..".png"},
-		paramtype = "light",
-		paramtype2 = "facedir",
-		groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
-		sounds = default.node_sound_wood_defaults(),
-		node_box = {
-			type = "fixed",
-			fixed = {
-						--legs
-						{-0.4375, -0.5, 0.375, -0.375, -0.375, 0.4375},
-						{0.375, -0.5, 0.375, 0.4375, -0.375, 0.4375},
-
-						--base/cushion
-						{-0.5, -0.375, -0.5, 0.5, 0, 0.5},
-
-						--back
-						{-0.5, 0, -0.5, -0.3125, 0.5, 0.5},
-
-						--arm
-						{-0.3125, 0, 0.3125, 0.5, 0.25, 0.5},
-					}
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-						{0, 0, 0, 0, 0, 0},
-					}
-		},
-	})
-
-	minetest.register_alias("lrfurn:sofa_"..colour, "lrfurn:sofa_right_"..colour)
+	minetest.register_alias("lrfurn:sofa_left_"..colour, "air")
+	minetest.register_alias("lrfurn:sofa_right_"..colour, "lrfurn:sofa_"..colour)
 
 	minetest.register_craft({
 		output = "lrfurn:sofa_"..colour,
