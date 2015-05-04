5c3699eba12285cc116b4655e41cae7e6255d1ee
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2015-05-04 02:16:36 -0400

    use mesh node for medicine cabinet (allows better usage of textures, and fewer thereof)

--- homedecor/bathroom_furniture.lua
+++ homedecor/bathroom_furniture.lua
@@ -60,20 +60,13 @@ homedecor.register("towel_rod", {
 
 homedecor.register("medicine_cabinet", {
 	description = S("Medicine Cabinet"),
+	mesh = "homedecor_medicine_cabinet.obj",
 	tiles = {
-		'homedecor_medicine_cabinet_tb.png',
-		'homedecor_medicine_cabinet_tb.png',
-		'homedecor_medicine_cabinet_sides.png',
-		'homedecor_medicine_cabinet_sides.png',
-		'homedecor_medicine_cabinet_back.png',
-		'homedecor_medicine_cabinet_front.png'
+		'default_wood.png',
+		'homedecor_medicine_cabinet_mirror.png'
 	},
 	inventory_image = "homedecor_medicine_cabinet_inv.png",
 	selection_box = {
-		type = "fixed",
-		fixed = {-0.3125, -0.1875, 0.3125, 0.3125, 0.5, 0.5}
-	},
-	node_box = {
 		type = "fixed",
 		fixed = {-0.3125, -0.1875, 0.3125, 0.3125, 0.5, 0.5}
 	},
@@ -90,22 +83,17 @@ homedecor.register("medicine_cabinet", {
 })
 
 homedecor.register("medicine_cabinet_open", {
+	mesh = "homedecor_medicine_cabinet_open.obj",
 	tiles = {
-		'homedecor_medicine_cabinet_tb.png',
-		'homedecor_medicine_cabinet_tb.png',
-		"homedecor_medicine_cabinet_open_right.png",
-		'homedecor_medicine_cabinet_sides.png',
-		'homedecor_medicine_cabinet_back.png',
-		"homedecor_medicine_cabinet_open_front.png"
+		'default_wood.png',
+		'homedecor_medicine_cabinet_mirror.png',
+		'homedecor_medicine_cabinet_inside.png'
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3125, -0.1875, -0.25, 0.3125, 0.5, 0.5}
 	},
 	groups = { snappy = 3, not_in_creative_inventory=1 },
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.3125, -0.1875, 0.375, 0.3125, 0.5, 0.5}, -- NodeBox1
-			{0.28, -0.1875, -0.1875, 0.3125, 0.5, 0.375}, -- NodeBox2
-		}
-	},
 	drop = "homedecor:medicine_cabinet",
 	on_punch = function(pos, node, puncher, pointed_thing)
 		node.name = "homedecor:medicine_cabinet"
