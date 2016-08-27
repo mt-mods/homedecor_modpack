mt-mods/homedecor_modpack

commit 436fdfac5520f991c26ff7edbf7806823be02981

Prevent crashs in can_dig on nil-player.

inbox/init.lua
@@ -61,9 +61,10 @@ minetest.register_node("inbox:empty", {
 	end,
 	can_dig = function(pos,player)
 		local meta = minetest.get_meta(pos);
+		local name = player and player:get_player_name()
 		local owner = meta:get_string("owner")
 		local inv = meta:get_inventory()
-		return player:get_player_name() == owner and inv:is_empty("main")
+		return player == owner and inv:is_empty("main")
 	end,
 	on_metadata_inventory_put = function(pos, listname, index, stack, player)
 		local meta = minetest.get_meta(pos)

itemframes/init.lua
@@ -141,9 +141,9 @@ minetest.register_node("itemframes:frame",{
 		end
 	end,
 	can_dig = function(pos,player)
-
+		local name = player and player:get_player_name()
 		local meta = minetest.get_meta(pos)
-		return player:get_player_name() == meta:get_string("owner")
+		return name == meta:get_string("owner")
 	end,
 	on_destruct = function(pos)
 		local meta = minetest.get_meta(pos)
@@ -198,9 +198,9 @@ minetest.register_node("itemframes:pedestal",{
 		end
 	end,
 	can_dig = function(pos,player)
-
+		local name = player and player:get_player_name()
 		local meta = minetest.get_meta(pos)
-		return player:get_player_name() == meta:get_string("owner")
+		return name == meta:get_string("owner")
 	end,
 	on_destruct = function(pos)
 		local meta = minetest.get_meta(pos)

commit 32ef2377d4e7fc75166d4cb0525dbbf0aeeff92c

fix wrong reference introduced in 436fdfac causing can_dig for mailboxes always to fail

inbox/init.lua
@@ -64,7 +64,7 @@ minetest.register_node("inbox:empty", {
 		local name = player and player:get_player_name()
 		local owner = meta:get_string("owner")
 		local inv = meta:get_inventory()
-		return player == owner and inv:is_empty("main")
+		return name == owner and inv:is_empty("main")
 	end,
 	on_metadata_inventory_put = function(pos, listname, index, stack, player)
 		local meta = minetest.get_meta(pos)