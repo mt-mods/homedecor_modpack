f90834ff0612a228dae3138b9ab89a34fb97828f
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2019-08-21 06:34:37 -0400

    make "computer" mod depend on basic_materials

--- computer/init.lua
+++ computer/init.lua
@@ -94,6 +94,5 @@ end
 
 local MODPATH = minetest.get_modpath("computer")
 dofile(MODPATH.."/computers.lua")
-dofile(MODPATH.."/miscitems.lua")
 dofile(MODPATH.."/recipes.lua")
 dofile(MODPATH.."/tetris.lua")
