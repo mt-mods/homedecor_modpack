cab9df225652c093d86dee6ab75c7f9cd4e48a67
unknown <24964441+wsor4035@users.noreply.github.com>
2022-04-24 11:14:56 -0400

    add hd furniture

--- homedecor_furniture/init.lua
+++ homedecor_furniture/init.lua
@@ -5,16 +5,21 @@ local ob_cbox = {
 	fixed = { -0.5, -0.5, 0, 0.5, 0.5, 0.5 }
 }
 
-minetest.register_node(":homedecor:openframe_bookshelf", {
+local wood_tex = "default_wood.png"
+if not minetest.get_modpath("default") then wood_tex = "[combine:16x16^[noalpha^[colorize:#654321" end
+
+homedecor.register("openframe_bookshelf", {
 	description = S("Bookshelf (open-frame)"),
 	drawtype = "mesh",
 	mesh = "homedecor_openframe_bookshelf.obj",
 	tiles = {
 		"homedecor_openframe_bookshelf_books.png",
-		"default_wood.png"
+		wood_tex
 	},
 	groups = {choppy=3,oddly_breakable_by_hand=2,flammable=3},
-	sounds = default.node_sound_wood_defaults(),
+	_sound_def = {
+		key = "node_sound_wood_defaults",
+	},
 	paramtype = "light",
 	paramtype2 = "facedir",
 	selection_box = ob_cbox,
@@ -24,10 +29,12 @@ minetest.register_node(":homedecor:openframe_bookshelf", {
 homedecor.register("wall_shelf", {
 	description = S("Wall Shelf"),
 	tiles = {
-		"default_wood.png",
+		wood_tex,
 	},
-	groups = { snappy = 3 },
-	sounds = default.node_sound_wood_defaults(),
+	groups = { snappy = 3, dig_tree = 2 },
+	_sound_def = {
+		key = "node_sound_wood_defaults",
+	},
 	node_box = {
 		type = "fixed",
 		fixed = {
