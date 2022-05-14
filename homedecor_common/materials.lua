mt-mods/homedecor_modpack

commit 546cd7110fe658a030d50a959283984e0660a915

make homedecor game agnostic (#28)
* make materials.lua with hades, minetest game, farlands items

* make textures file for central handling

* make mods game agnostic

* take out the trash

* handle multiple seats in sofas

* add github luacheck workflow and various luacheck fixes

* add fluxionary scope creep

* fix devtest breaking right to a name policy

Co-authored-by: wsor4035 <24964441+wsor4035@users.noreply.github.com>
Co-authored-by: SFENCE <sfence.software@gmail.com>

homedecor_common/materials.lua
@@ -1,0 +1,212 @@
+homedecor.materials = {
+    dirt = "default:dirt",
+    sand = "default:sand",
+    gravel = "default:gravel",
+    copper_ingot = "default:copper_ingot",
+    steel_ingot = "default:steel_ingot",
+    gold_ingot = "default:gold_ingot",
+    tin_ingot = "default:tin_ingot",
+    mese_crystal_fragment = "default:mese_crystal_fragment",
+    torch = "default:torch",
+    diamond = "default:diamond",
+    clay_lump = "default:clay_lump",
+    water_bucket = "bucket:bucket_water",
+    empty_bucket = "bucket:bucket_empty",
+    dye_dark_grey = "dye:dark_grey",
+    dye_black = "dye:black",
+    dye_white = "dye:white",
+    dye_green = "dye:green",
+    dye_red = "dye:red",
+    dye_yellow = "dye:yellow",
+    dye_brown = "dye:brown",
+    dye_blue = "dye:blue",
+    dye_violet = "dye:violet",
+    dye_grey = "dye:grey",
+    dye_dark_green = "dye:dark_green",
+    dye_orange = "dye:orange",
+    dye_pink = "dye:pink",
+    silicon = "mesecons_materials:silicon",
+    string = "farming:string",
+    paper = "default:paper",
+    book = "default:book",
+    iron_lump = "default:iron_lump",
+    wool_grey = "wool:grey",
+    wool_green = "wool:green",
+    wool_dark_green = "wool:dark_green",
+    wool_brown = "wool:brown",
+    wool_black = "wool:black",
+    wool_white = "wool:white",
+    slab_stone = "stairs:slab_stone",
+    slab_wood = "stairs:slab_wood",
+    glass_block = "default_glass",
+    glass_bottle = "vessels:glass_bottle",
+    coal_lump = "default:coal_lump",
+    stone = "default:stone",
+    cobble = "default:cobble",
+    brick = "default:brick",
+}
+
+if minetest.get_modpath("moreores") then
+    homedecor.materials.silver_ingot = "moreores:silver_ingot"
+end
+
+if minetest.get_modpath("mcl_core") then
+    homedecor.materials = {
+        dirt = "mcl_core:dirt",
+        sand = "mcl_core:sand",
+        gravel = "mcl_core:gravel",
+        copper_ingot = "mcl_copper:copper_ingot",
+        steel_ingot = "mcl_core:iron_ingot",
+        gold_ingot = "mcl_core:gold_ingot",
+        tin_ingot = "mcl_core:iron_ingot",
+        mese_crystal_fragment = "mcl_core:iron_ingot",
+        torch = "mcl_torches:torch",
+        diamond = "mcl_core:diamond",
+        clay_lump = "default:clay_lump",
+        water_bucket = "mcl_buckets:bucket:bucket_water",
+        empty_bucket = "mcl_buckets:bucket_empty",
+        dye_dark_grey = "mcl_dyes:dark_grey",
+        dye_black = "mcl_dyes:black",
+        dye_white = "mcl_dyes:white",
+        dye_green = "mcl_dyes:green",
+        dye_red = "mcl_dyes:red",
+        dye_yellow = "mcl_dyes:yellow",
+        dye_brown = "mcl_dyes:brown",
+        dye_blue = "mcl_dyes:blue",
+        dye_violet = "mcl_dyes:violet",
+        dye_grey = "mcl_dyes:grey",
+        dye_dark_green = "mcl_dyes:dark_green",
+        dye_orange = "mcl_dyes:orange",
+        dye_pink = "mcl_dyes:pink",
+        silicon = "mcl_core:iron_ingot",
+        string = "mcl_mobitems:string",
+        paper = "mcl_core:paper",
+        book = "mcl_core:book",
+        iron_lump = "mcl_core:stone_with_iron",
+        wool_grey = "mcl_wool:grey",
+        wool_green = "mcl_wool:green",
+        wool_dark_green = "mcl_wool:dark_green",
+        wool_brown = "mcl_wool:brown",
+        wool_black = "mcl_wool:black",
+        wool_white = "mcl_wool:white",
+        slab_stone = "mcl_stairs:slab_stone",
+        slab_wood = "mcl_stairs:slab_wood",
+        glass_block = "mcl_core:glass",
+        glass_bottle = "mcl_core:glass_bottle",
+        coal_lump = "mcl_core:coal",
+        stone = "mcl_core:stone",
+        cobble = "mcl_core:cobble",
+        brick = "mcl_core:brick",
+    }
+elseif minetest.get_modpath("fl_ores") and minetest.get_modpath("fl_stone") then
+    homedecor.materials = {
+        dirt = "fl_topsoil:dirt",
+        sand = "fl_stone:sand",
+        gravel = "fl_topsoil:gravel",
+        steel_ingot = "fl_ores:iron_ingot",
+        gold_ingot = "fl_ores:gold_ingot",
+        mese_crystal_fragment = "fl_ores:iron_ingot",
+        torch = "fl_light_sources:torch",
+        diamond = "fl_ores:diamond",
+        clay_lump = "fl_bricks:clay_lump",
+        water_bucket = "fl_bucket:bucket_water",
+        empty_bucket = "fl_bucket:bucket",
+        dye_dark_grey = "fl_dyes:dark_grey_dye",
+        dye_black = "fl_dyes:black_dye",
+        dye_white = "fl_dyes:white_dye",
+        dye_green = "fl_dyes:green_dye",
+        dye_red = "fl_dyes:red_dye",
+        dye_yellow = "fl_dyes:yellow_dye",
+        dye_brown = "fl_dyes:brown_dye",
+        dye_blue = "fl_dyes:blue_dye",
+        dye_violet = "fl_dyes:violet_dye",
+        dye_grey = "fl_dyes:grey_dye",
+        dye_dark_green = "fl_dyes:dark_green_dye",
+        dye_orange = "fl_dyes:orange_dye",
+        dye_pink = "fl_dyes:pink_dye",
+        copper_ingot = "fl_ores:copper_ingot",
+        tin_ingot = "fl_ores:tin_ingot",
+        silver_ingot = "fl_ores:iron_ingot",
+        silicon = "mesecons_materials:silicon",
+        string = "fl_plantlife:oxeye_daisy",
+        paper = "basic_materials:plastic_sheet",
+        iron_lump = "fl_ores:iron_ore",
+        wool_grey = "",
+        wool_green = "",
+        wool_dark_green = "",
+        wool_brown = "",
+        wool_black = "",
+        wool_white = "",
+        slab_stone = "fl_stone:stone_slab",
+        slab_wood = "fl_trees:apple_plank_slab",
+        glass_block = "fl_glass:framed_glass",
+        glass_bottle = "fl_bottles:bottle",
+        coal_lump = "fl_ores:coal_ore",
+        stone = "fl_stone:stone",
+        cobble = "fl_stone:stone_rubble",
+        brick = "",
+    }
+elseif minetest.get_modpath("hades_core") then
+    homedecor.materials = {
+        sand = "hades_core:fertile_sand",
+        gravel = "hades_core:gravel",
+        copper_ingot = "hades_core:copper_ingot",
+        steel_ingot = "hades_core:steel_ingot",
+        gold_ingot = "hades_core:gold_ingot",
+        tin_ingot = "hades_core:tin_ingot",
+        silver_ingot = "--unknown--",
+        mese_crystal_fragment = "hades_core:mese_crystal_fragment",
+        torch = "hades_torches:torch",
+        diamond = "hades_core:diamond",
+        clay_lump = "hades_core:clay_lump",
+        clay_brick = "hades_core:clay_brick",
+
+        --[[
+            Since hades doesnt have buckets or water for the user,
+            using dirt from near water to pull the water out
+        ]]
+        water_bucket = "hades_core:dirt",
+        empty_bucket = "hades_core:fertile_sand",
+        dye_dark_grey = "dye:dark_grey",
+        dye_black = "dye:black",
+        dye_white = "dye:white",
+        dye_green = "dye:green",
+        dye_red = "dye:red",
+        dye_yellow = "dye:yellow",
+        dye_brown = "dye:brown",
+        dye_blue = "dye:blue",
+        dye_violet = "dye:violet",
+        dye_grey = "dye:grey",
+        dye_dark_green = "dye:dark_green",
+        dye_orange = "dye:orange",
+        dye_pink = "dye:pink",
+        silicon = "hades_materials:silicon",
+        string = "hades_farming:string",
+        paper = "hades_core:paper",
+        book = "hades_core:book",
+        iron_lump = "hades_core:iron_lump",
+        wool_grey = "wool:grey",
+        wool_green = "wool:green",
+        wool_dark_green = "wool:dark_green",
+        wool_brown = "wool:brown",
+        wool_black = "wool:black",
+        wool_white = "wool:white",
+        slab_stone = "stairs:slab_stone",
+        slab_wood = "stairs:slab_wood",
+        glass_block = "hades_core:glass",
+        glass_bottle = "vessels:glass_bottle",
+        obsidian_glass = "hades_core:obsidian_glass",
+        coal_lump = "hades_core:coal_lump",
+        stone = "hades_core:stone",
+        cobble = "hades_core:cobble",
+        brick = "hades_core:brick"
+    }
+
+    if minetest.get_modpath("hades_bucket") then
+        homedecor.materials["water_bucket"] = "hades_bucket:bucket_water"
+        homedecor.materials["empty_bucket"] = "hades_bucket:bucket_empty"
+    end
+    if minetest.get_modpath("hades_extraores") then
+        homedecor.materials["silver_ingot"] = "hades_extraores:silver_ingot"
+    end
+end

homedecor_common/registration.lua
@@ -46,9 +46,12 @@ function homedecor.register(name, original_def)
 	if expand then
 		-- dissallow rotating only half the expanded node by default
 		-- unless we know better
-		def.on_rotate = def.on_rotate
-			or (minetest.get_modpath("screwdriver") and (def.mesh and expand.top and screwdriver.rotate_simple)
-			or screwdriver.disallow) or nil
+		def.on_rotate = def.on_rotate or (
+			minetest.get_modpath("screwdriver") and (
+				(def.mesh and expand.top and screwdriver.rotate_simple) or
+				screwdriver.disallow
+			)
+		)
 
 		def.on_place = def.on_place or function(itemstack, placer, pointed_thing)
 			if expand.top then
@@ -96,6 +99,31 @@ function homedecor.register(name, original_def)
 		end
 	end
 
+	local crafts = def.crafts and table.copy(def.crafts) or {}
+	def.crafts = nil
+
 	-- register the actual minetest node
 	minetest.register_node(":homedecor:" .. name, def)
+
+	for _, cdef in pairs(crafts) do
+		if cdef.recipe then
+			for k, row in pairs(cdef.recipe) do
+				if type(row) == "string" and homedecor.materials[row] then
+					cdef.recipe[k] = homedecor.materials[row]
+				elseif type(row) == "table" then
+					for i, item in pairs(row) do
+						if homedecor.materials[item] then
+							cdef.recipe[k][i] = homedecor.materials[item]
+						end
+					end
+				end
+			end
+		end
+
+		if cdef.type ~= "toolrepair" and not cdef.output then
+			cdef.output = ":homedecor:" .. name
+		end
+
+		minetest.register_craft(cdef)
+	end
 end

commit 70381161fd1b9b8ecab5d40fe157a14a2cbb773a

fix recipes broken by 546cd711
fixes #35

homedecor_common/materials.lua
@@ -38,7 +38,7 @@ homedecor.materials = {
     wool_white = "wool:white",
     slab_stone = "stairs:slab_stone",
     slab_wood = "stairs:slab_wood",
-    glass_block = "default_glass",
+    glass_block = "default:glass",
     glass_bottle = "vessels:glass_bottle",
     coal_lump = "default:coal_lump",
     stone = "default:stone",

homedecor_common/registration.lua
@@ -121,7 +121,7 @@ function homedecor.register(name, original_def)
 		end
 
 		if cdef.type ~= "toolrepair" and not cdef.output then
-			cdef.output = ":homedecor:" .. name
+			cdef.output = "homedecor:" .. name
 		end
 
 		minetest.register_craft(cdef)